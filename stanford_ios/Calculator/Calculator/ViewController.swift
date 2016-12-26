e97b995653b122f896daab0d3c7206c490657b13
heference <leiceo87@gmail.com>
2016-12-26 22:42:27 +0900

    Stanford Lecture Code

---
+++ stanford_ios/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Ikarious on 2016. 12. 26..
+//  Copyright © 2016년 heference. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func touchDigit(_ sender: UIButton) {
+        print("touchDigit")
+    }
+    
+}
+
