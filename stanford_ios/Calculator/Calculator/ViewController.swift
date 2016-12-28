ae156d75f0ad106e9d84dd69e1c64cda8b91bfd4
heference <leiceo87@gmail.com>
2016-12-28 22:31:44 +0900

    iOS stanford 강좌 예제소스 작성

--- stanford_ios/Calculator/Calculator/ViewController.swift
+++ stanford_ios/Calculator/Calculator/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel?
+    
+    var userIsInTheMiddleOfTyping: Bool
+    
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
+        let textCurrentlyInDisplay = display!.text!
+        display!.text = textCurrentlyInDisplay + digit
+        
         print("touched \(digit) digit")
     }
     
